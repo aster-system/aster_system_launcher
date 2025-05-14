9f5580ea66d0545c59dad06a78da2e8bf0fd6493
Matt Mmo <mmattmmo@gmail.com>
2025-05-14 22:07:05 +0200

    Update to the new SCLS version

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ using namespace std;
 SCLS_INIT
 
 int main(int argc, char* argv[]) {
+    scls::Socket socket = scls::Socket("tarn.mon-ent-occitanie.fr", 80);
+    int result = socket.connect();
+    socket.send_datas("GET / HTTP/1.1\r\nConnection: close\r\nHost: tarn.mon-ent-occitanie.fr\r\n\r\n");
+    std::cout << "E " << socket.receive_datas().datas.get()->extract_string_all() << std::endl;
+
+    /*
     as_launcher::AS_Window window(900, 600, argv[0]);
     window.load_from_xml("assets/window.txt");
 
